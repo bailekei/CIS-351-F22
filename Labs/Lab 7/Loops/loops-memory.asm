2d7d5023545062132d912cd4f27bdc393436e451
bailekei <bailekei@mail.gvsu.edu>
2022-11-21 16:59:11 -0500

    added lab

--- Labs/Lab 7/Loops/loops-memory.asm
+++ Labs/Lab 7/Loops/loops-memory.asm
@@ -1,17 +1,17 @@
 .globl max sum13
+.data 
+	example_arr: .word 2 4 6 8 10
 .text
 # Keilani Bailey and Sebastian Torrejon Alonzo
 # ----------------------------------------------------------------------------
-	addi $a0, $0, 1		
-	addi $a1, $0, 1
-	jal max	
+.text
 
-	addi $a0, $0, 1
-	addi $a1, $0, 1
-	jal sum13
+# la $a0, example_arr
+# addi $a1, $0, 5
+# jal sum13
+# addi $v0, $0, 10
+# syscall
 
-	addi $v0, $0, 10	
-	syscall			
 # ----------------------------------------------------------------------------
 # Return the largest value in array
 #    int i;
@@ -23,40 +23,36 @@
 #            max = arr[i];
 #  
 #    return max;
-
+# ----------------------------------------------------------------------------	
 max: 
-	addi $s0, $zero, 0 #i = 0
-	add $t0, $t0, $a0 # address of array
-	lw $t1, 0($t0) # t1 = array[0]
-	add $t2, $t1, $zero # max/t2 = arr[0]
-	sw $t1, 0($t0)
+	addi $t0, $0, 1 # i = 1
+	addi $t1, $0, 0 # max = 0
 	
-	addi $s0, $zero, 1 # i = 0
-	j loop
-	add $v0, $zero, $t2 # v0 = max/t2
+	la $t3, ($a0) # load address of array
+	lw $t2, 0($t3) # a[0]
+	add $t1, $t1, $t2
 	
-	jr $ra
 	
-	loop:
-		beq $s0, $a1, exit
-		add $t0, $t0, $a0
-		lw $t1, 0($t0)
-		sgt $t3, $t1, $t2 # arr[i] > max
-		beq $t3, 1, set_max
-		sw $t1, 0($t0)
+	for_loop:
+		slt $t4, $t0, $a1 # i < n
+		beq $t4, $0, exit
+		sll $t4, $t0, 2 # t4 = i*4
+		add $t4, $t4, $t3 # t4 = (i*4) + array address
+		lw $t5, 0($t4) # a[i]
+		bgt $t5, $t1, if_state
 		
-		addi $s0, $s0, 1
-		j loop
-	
-	set_max:
-		add $t2, $t1, $zero
-		j loop
+		addi $t0, $t0, 1 # increment i
+		j for_loop
+		
+	if_state:
+		add $t1, $0, $t5 # max = arr[i]
+		j for_loop
 		
 	exit:
+		add $v0, $0, $t1 # v0 = max
 		jr $ra
-		# leave loop
 
-
+# ----------------------------------------------------------------------------		
 # public int sum13(int[] nums, int size) {
 #    int sum = 0;
 #    int i = 0;
@@ -71,23 +67,29 @@ max:
 #    }
 #    return sum;
 # }
+# ----------------------------------------------------------------------------	
 
 sum13:
-	addi $t0, $zero, 0 # sum = 0
-	addi $t1, $zero, 0 # i = 0
-	jr $ra
+	addi $t0, $0, 0 # sum = 0
+	addi $t1, $0, 0 # i = 0
+	la $t2, ($a0) # address of array
+	j while_loop
 	
-	while:
-		beq $t1, $a1, exit # i < num.length
-		beq $t3, 13, if_13 # if nums[i] == 13
+	while_loop:
+		bge $t1, $a1, leave # i < 0
+		sll $t3, $t1, 2 # t3 = i*4
+		add $t3, $t3, $t2 # t3 = (i*4) + array address
+		lw $t4, 0($t3) # a[i]
+		beq $t4, 13, if_13
 		
-		# else
-		add $t0, $t0, $zero # sum = sum + nums[i]
-		addi $t1, $t1, 1
-		
-		j while
-		
-		
+		add $t0, $t0, $t4 # sum += arr[i]
+		addi $t1, $t1, 1 # increment i
+		j while_loop
+	
 	if_13:
-		addi $t1, $t1, 2
-		j while
+		addi $t1, $t1, 2 # i+=2
+		j while_loop
+	leave:
+		add $v0, $0, $t0
+		jr $ra
+	
